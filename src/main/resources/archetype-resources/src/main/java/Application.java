76387699b5ce8bba95a0a884898ee3391bca1817
Muhammad Salman <m.salman86@gmail.com>
2016-02-12 17:23:20 -0500

    formatted file spacing and added editorconfig

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,12 +1,14 @@
-package ${package};
+package $
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+{package};
+
+  import org.springframework.boot.autoconfigure.SpringBootApplication;
+  import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).run(args);
-	}
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class).run(args);
+  }
 }
--- src/main/resources/archetype-resources/src/main/java/util/LoggerUtil.java
+++ src/main/resources/archetype-resources/src/main/java/util/LoggerUtil.java
@@ -1,7 +1,10 @@
-package ${package}.util;
+package $
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+{package}.util;
+
+  import org.slf4j.Logger;
+  import org.slf4j.LoggerFactory;
+  import org.springframework.boot.logging.LogLevel;
 
 /**
  * A logger utility class
@@ -9,139 +12,118 @@ import org.slf4j.LoggerFactory;
 
 public class LoggerUtil {
 
-    public static <T> void logServiceError(Class<T> clazz, String methodName, Throwable ex, Object... objects) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.error("Exception in Service Layer: {}({}) - Original Exception was {}", methodName, objects, ex);
+  private static Class getCallerClass() {
+    try {
+      String callerClassName = Thread.currentThread().getStackTrace()[3].getClassName();
+      return Class.forName(callerClassName);
+    } catch (ClassNotFoundException e) {
+      Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+      logger.error("Exception ", e);
     }
+    return LoggerUtil.class;
+  }
 
-    public static <T> void logError(Class<T> clazz, String message) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.error(message);
+  public static void logError(String message) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.error(message);
+  }
+
+  public static void logError(String message, Object... params) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.error(message, params);
+  }
+
+  public static void logInfo(String message) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.info(message);
+  }
+
+  public static void logInfo(String message, Object... params) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.info(message, params);
+  }
+
+  public static void logDebug(String message) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.debug(message);
+  }
+
+  public static void logDebug(String message, Object... params) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.debug(message, params);
+  }
+
+  public static void logError(String message, Throwable ex) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.error(message, ex);
+  }
+
+  public static void logInfo(String message, Throwable ex) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.info(message, ex);
+  }
+
+  public static void logDebug(String message, Throwable ex) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    logger.debug(message, ex);
+  }
+
+  public static long logEnter(LogLevel logType, String methodName) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("ENTER - {}()", methodName);
+    } else {
+      logger.debug("ENTER - {}()", methodName);
     }
+    return System.currentTimeMillis();
+  }
 
-    public static <T> void logError(Class<T> clazz, String message, Object... params) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.error(message, params);
+
+  public static long logEnter(LogLevel logType, String methodName, Object... params) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("ENTER - {}({})", methodName, params);
+    } else {
+      logger.debug("ENTER - {}({})", methodName, params);
     }
+    return System.currentTimeMillis();
+  }
 
-    public static <T> void logInfo(Class<T> clazz, String message) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.info(message);
+  public static void logExit(LogLevel logType, String methodName, long startTime) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    long runTime = System.currentTimeMillis() - startTime;
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("EXIT - {}() in {}ms", methodName, runTime);
+    } else {
+      logger.debug("EXIT - {}() in {}ms", methodName, runTime);
     }
+  }
 
-    public static <T> void logInfo(Class<T> clazz, String message, Object... params) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.info(message, params);
+  public static void logExit(LogLevel logType, String methodName) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("EXIT - {}()", methodName);
+    } else {
+      logger.debug("EXIT - {}()", methodName);
     }
+  }
 
-    public static <T> void logDebug(Class<T> clazz, String message) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.debug(message);
+  public static void logExit(LogLevel logType, String methodName, Object returnValue, long startTime) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    long runTime = System.currentTimeMillis() - startTime;
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("EXIT - {}() in {}ms > {}", methodName, runTime, returnValue);
+    } else {
+      logger.debug("EXIT - {}() in {}ms > ({})", methodName, runTime, returnValue);
     }
+  }
 
-    public static <T> void logDebug(Class<T> clazz, String message, Object... params) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.debug(message, params);
+  public static void logExit(LogLevel logType, String methodName, Object returnValue) {
+    Logger logger = LoggerFactory.getLogger(getCallerClass());
+    if (logType.equals(LogLevel.INFO)) {
+      logger.info("EXIT - {}() > {}", methodName, returnValue);
+    } else {
+      logger.debug("EXIT - {}() > ({})", methodName, returnValue);
     }
-
-    public static <T> void logError(Class<T> clazz, String message, Throwable ex) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.error(message, ex);
-    }
-
-    public static <T> void logInfo(Class<T> clazz, String message, Throwable ex) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.info(message, ex);
-    }
-
-    public static <T> void logDebug(Class<T> clazz, String message, Throwable ex) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.debug(message, ex);
-    }
-
-    /**
-     * Method to be used for logging start of Service Methods without input parameters
-     *
-     * @param methodName
-     */
-    public static <T> long logEnter(Class<T> clazz, String methodName) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-
-        logger.trace("ENTER: {}", methodName);
-        return System.currentTimeMillis();
-    }
-
-    public static <T> long logEnter(Class<T> clazz, String methodName, Object... params) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        logger.trace("ENTER: {}({})", methodName, params);
-        return System.currentTimeMillis();
-    }
-
-    /**
-     * Method to be used for logging start of Service Methods without input parameters
-     *
-     * @param logType
-     * @param methodName
-     */
-    public static <T> long logEnter(Class<T> clazz, LogLevel logType, String methodName) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        if (logType.equals(LogLevel.INFO)) {
-            logger.info("ENTER - {}()", methodName);
-        } else {
-            logger.debug("ENTER - {}()", methodName);
-        }
-        return System.currentTimeMillis();
-    }
-
-
-    public static <T> long logEnter(Class<T> clazz, LogLevel logType, String methodName, Object... params) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        if (logType.equals(LogLevel.INFO)) {
-            logger.info("ENTER - {}({})", methodName, params);
-        } else {
-            logger.debug("ENTER - {}({})", methodName, params);
-        }
-        return System.currentTimeMillis();
-    }
-
-    public static <T> void logExit(Class<T> clazz, String methodName, long startTime) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        long runTime = System.currentTimeMillis() - startTime;
-        logger.trace("EXIT: {} in {}ms", methodName, runTime);
-    }
-
-    public static <T> void logExit(Class<T> clazz, String methodName, Object returnValue, long startTime) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        long runTime = System.currentTimeMillis() - startTime;
-        logger.trace("EXIT: {} in {}ms > {}", methodName, runTime, returnValue);
-    }
-
-    public static <T> void logExit(Class<T> clazz, LogLevel logType, String methodName, long startTime) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        long runTime = System.currentTimeMillis() - startTime;
-        if (logType.equals(LogLevel.INFO)) {
-            logger.info("EXIT - {}() in {}ms", methodName, runTime);
-        } else {
-            logger.debug("EXIT - {}() in {}ms", methodName, runTime);
-        }
-    }
-
-    public static <T> void logExit(Class<T> clazz, LogLevel logType, String methodName) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        if (logType.equals(LogLevel.INFO)) {
-            logger.info("EXIT - {}()", methodName);
-        } else {
-            logger.debug("EXIT - {}()", methodName);
-        }
-    }
-
-    public static <T> void logExit(Class<T> clazz, LogLevel logType, String methodName, Object returnValue, long startTime) {
-        Logger logger = LoggerFactory.getLogger(clazz);
-        long runTime = System.currentTimeMillis() - startTime;
-        if (logType.equals(LogLevel.INFO)) {
-            logger.info("EXIT - {}() in {}ms > {}", methodName, runTime, returnValue);
-        } else {
-            logger.debug("EXIT - {}() in {}ms > {}", methodName, runTime, returnValue);
-        }
-    }
+  }
 }
